0b8fa0d04a526eca7233bf8c20b4148299f55fb5
Arsh77 <arshabhsemwal9528@gmail.com>
2020-08-17 01:25:41 +0530

    url shortning REST

---
+++ url_shortner/src/main/java/com/url/shortner/urlshortner/entity/PairedUrl.java
@@ -0,0 +1,29 @@
+package com.url.shortner.urlshortner.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="PAIRED_URL")
+public class PairedUrl {
+	@Id
+	@Column(name="id", nullable=false)
+	private String id;
+	
+	@Column(name="shorturl", unique=true)
+	private String shorturl;
+	
+	@Column(name="longurl", nullable=false)
+	private String longurl;
+	
+	
+	
+}
